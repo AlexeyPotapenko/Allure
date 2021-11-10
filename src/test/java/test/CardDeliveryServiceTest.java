511dceca2520e366a26634cca88171fe3bc85780
AlexeyPotapenko <alesha_potapenko@mail.ru>
2021-11-10 15:03:53 +0300

    add check visible exact text

--- src/test/java/test/CardDeliveryServiceTest.java
+++ src/test/java/test/CardDeliveryServiceTest.java
@@ -25,13 +25,13 @@ public class CardDeliveryServiceTest {
         $("[data-test-id=agreement]").click();
         $(Selectors.byText("Запланировать")).click();
         $(withText("Успешно!")).shouldBe(visible);
-        $("div.notification__content").shouldBe(exactText("Встреча успешно запланирована на " + DataGenerator.generateDate(3)));
+        $("div.notification__content").shouldBe(visible,exactText("Встреча успешно запланирована на " + DataGenerator.generateDate(3)));
         $("[class='input__control'][placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[class='input__control'][placeholder='Дата встречи']").setValue(DataGenerator.generateDate(5));
         $(Selectors.byText("Запланировать")).click();
         $(withText("Необходимо подтверждение")).shouldBe(visible, Duration.ofSeconds(15));
         $(Selectors.byText("Перепланировать")).click();
-        $("div.notification__content").shouldBe(exactText("Встреча успешно запланирована на " + DataGenerator.generateDate(5)));
+        $("div.notification__content").shouldBe(visible,exactText("Встреча успешно запланирована на " + DataGenerator.generateDate(5)));
 
 
     }
